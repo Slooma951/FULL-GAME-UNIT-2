b955a46b9a29742903fe4d9e07fbbd722300d9f8
Slooma951 <146085778+Slooma951@users.noreply.github.com>
2023-10-15 15:23:49 +0100

    Add files via upload

---
+++ Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float fireRate = 1.0f;
+    private float nextFireTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the spacebar was pressed
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnXLeft = -22;
+    private float spawnXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    
+    void Start()
+    {
+   
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at the top of the play area
+    void SpawnRandomBall()
+    {
+        spawnInterval = Random.Range(3, 5);
+
+        int index = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnXLeft, spawnXRight), spawnPosY, 0);
+
+        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
+    }
+}
